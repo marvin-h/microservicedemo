733b3bd806e589f6f5f8187288e9381c9bfc4ffc
Huang <min.huang02@sap.com>
2016-07-20 16:22:07 +0800

    init

--- salesorder/salesorder-srv/src/main/java/com/demo/microservicedemo/salesorder/app/App.java
+++ salesorder/salesorder-srv/src/main/java/com/demo/microservicedemo/salesorder/app/App.java
@@ -4,13 +4,14 @@ import com.demo.microservicedemo.salesorder.dto.SalesOrder;
 import com.demo.microservicedemo.salesorder.service.SalesOrderService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context = new ClassPathXmlApplicationContext("jpa.xml");
         SalesOrderService salesOrderService = context.getBean(SalesOrderService.class);
         SalesOrder salesOrder = new SalesOrder();
         salesOrder = salesOrderService.createSalesOrder(salesOrder);
-        System.out.println(salesOrder);
+        System.out.println(salesOrder.getId());
     }
 }
